8c3dde6588c5b94bbf692811b496525ef32f8a25
Appoline Cogan <appolinecogan@gmail.com>
2022-08-09 14:25:01 +0700

    updated employee, inter, engineer and manager classes

--- index.js
+++ index.js
@@ -3,35 +3,6 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
-const questions = [
-  {
-    type: "input",
-    message: "What is name of the team's manager?",
-    name: "managerName",
-  },
-  {
-    type: "input",
-    message: "What is the epmloyee ID of the manager?",
-    name: "managerId",
-  },
-  {
-    type: "input",
-    message: "What is the email address of the manager?",
-    name: "managerEmail",
-  },
-  {
-    type: "input",
-    message: "What is the office number of the manager?",
-    name: "managerNumber",
-  },
-  {
-    type: "list",
-    message: "Please add a new employee",
-    name: "adding",
-    choices: ["Engineer", "Intern", "I am finished building my team"],
-  },
-];
-
 //function to write html file
 function writeToFile(data) {
   return fs.writeFileSync(
